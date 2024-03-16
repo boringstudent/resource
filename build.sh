de639e05ec054d3671a8068f5458055f62b0e7ae
CSneko <minecrafthetun@outlook.com>
2024-03-16 16:36:11 +0800

    添加构建脚本

--- build.sh
+++ build.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-# 获取当前目录
-CURRENT_DIR=$(pwd)
+# 获取脚本所在的相对目录
+SCRIPT_DIR=$(dirname "$0")
 
-# 构建文件和文件夹列表
-FILES_AND_DIRS=$(ls -1)
-
-# 构建 HTML 内容
-HTML_CONTENT="<!DOCTYPE HTML>
-<html>
+# 生成目录索引的函数
+generate_index() {
+    local dir=$1
+    local content="<html>
 <head>
 <meta charset=\"utf-8\">
-<title>$CURRENT_DIR</title>
+<title>$dir</title>
 </head>
 <body>
-<h1>$CURRENT_DIR</h1>
+<h1>$dir</h1>
 <hr>"
+    
+    # 添加上级目录链接
+    if [[ "$dir" != "$SCRIPT_DIR" ]]; then
+        content+="\n<a href=\"../\">../</a><br>"
+    fi
 
-# 添加上级目录链接
-HTML_CONTENT+="\n<a href=\"../\">../</a><br>"
+    # 遍历目录中的文件和文件夹，添加链接
+    local item
+    for item in "$dir"/*; do
+        item=${item##*/}
+        content+="\n<a href=\"$item\">$item</a><br>"
+    done
 
-# 遍历文件和文件夹，添加链接
-while IFS= read -r ITEM; do
-    HTML_CONTENT+="\n<a href=\"$ITEM\">$ITEM</a><br>"
-done <<< "$FILES_AND_DIRS"
-
-HTML_CONTENT+="\n</body>
+    content+="\n</body>
 </html>"
+    echo "$content"
+}
 
-# 在当前目录及子目录下添加 index.html 文件
-find . -type d -exec sh -c 'echo "$0" > "$0/index.html"' {} \;
-
-# 在当前目录及子目录下添加 index.html 文件的内容
-find . -type f -name 'index.html' -exec sh -c "echo '$HTML_CONTENT' > {}" \;
+# 在当前目录及子目录下生成 index.html 文件
+find "$SCRIPT_DIR" -type d -exec sh -c 'generate_index "$0" > "$0/index.html"' {} \;
 
