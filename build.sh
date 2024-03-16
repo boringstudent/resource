ade66e5253931e939f4e2fa408cd279e85d3d37a
CSneko <minecrafthetun@outlook.com>
2024-03-16 17:05:08 +0800

    添加构建脚本

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
 # 获取脚本所在的相对目录
 SCRIPT_DIR=$(dirname "$0")
 
-# 在目录下生成 index.html 文件
-find "$SCRIPT_DIR" -type d -execdir sh -c '
-    local dir="$0"
+# 生成目录索引的函数
+generate_index() {
+    local dir=$1
     local content="<html>
 <head>
 <meta charset=\"utf-8\">
@@ -14,7 +12,7 @@ find "$SCRIPT_DIR" -type d -execdir sh -c '
 <body>
 <h1>$dir</h1>
 <hr>"
-
+    
     # 添加上级目录链接
     if [[ "$dir" != "$SCRIPT_DIR" ]]; then
         content+="\n<a href=\"../\">../</a><br>"
@@ -26,11 +24,8 @@ find "$SCRIPT_DIR" -type d -execdir sh -c '
         item=${item##*/}
         if [[ -d $item ]]; then
             content+="\n<a href=\"$item/\">$item</a>/ (Directory)<br>"
-            # 递归处理子目录（由于已经内联，不再需要函数调用）
-            (
-                cd "$item" || exit
-                content+="\n$(generate_subindex)\n"
-            )
+            # 递归调用生成子目录的索引
+            generate_index "$item"
         else
             content+="\n<a href=\"$item\">$item</a><br>"
         fi
@@ -38,24 +33,8 @@ find "$SCRIPT_DIR" -type d -execdir sh -c '
 
     content+="\n</body>
 </html>"
-    echo "$content" > index.html
-' {} \;
-
-generate_subindex() {
-    local subdir=$1
-    local subdir_content="<html>
-<head>
-<meta charset=\"utf-8\">
-<title>$subdir</title>
-</head>
-<body>
-<h1>$subdir</h1>
-<hr>"
-
-    # 在子目录中遍历文件和子目录
-    # ... （重复 generate_index 函数中处理子目录的部分逻辑）
-
-    subdir_content+="\n</body>
-</html>"
-    echo "$subdir_content"
+    echo "$content"
 }
+
+# 在目录下生成 index.html 文件
+find "$SCRIPT_DIR" -type d -execdir sh -c 'generate_index "$0" > index.html' {} \;
