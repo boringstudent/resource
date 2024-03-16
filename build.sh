700ea9776adffdc6cd068b0fbfe92dd11a9a5267
CSneko <minecrafthetun@outlook.com>
2024-03-16 17:24:29 +0800

    修改脚本

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+# 在目录下生成 index.html 文件
+find "$SCRIPT_DIR" -type d -execdir bash -c '
 # 获取脚本所在的相对目录
 SCRIPT_DIR=$(dirname "$0")
 
 # 生成目录索引的函数
-function generate_index {
+generate_index() {
     local dir=$1
     local content="<html>
 <head>
@@ -15,7 +19,7 @@ function generate_index {
     
     # 添加上级目录链接
     if [[ "$dir" != "$SCRIPT_DIR" ]]; then
-        content+="\n<a href=\"../\">../</a><br>"
+        content+="<a href=\"../\">../</a><br>"
     fi
 
     # 遍历目录中的文件和文件夹，添加链接
@@ -23,18 +27,16 @@ function generate_index {
     for item in "$dir"/*; do
         item=${item##*/}
         if [[ -d $item ]]; then
-            content+="\n<a href=\"$item/\">$item</a>/ (Directory)<br>"
+            content+="<a href=\"$item/\">$item</a>/ (Directory)<br>"
             # 递归调用生成子目录的索引
             generate_index "$item"
         else
-            content+="\n<a href=\"$item\">$item</a><br>"
+            content+="<a href=\"$item\">$item</a><br>"
         fi
     done
 
-    content+="\n</body>
+    content+="</body>
 </html>"
     echo "$content"
 }
-
-# 在目录下生成 index.html 文件
-find "$SCRIPT_DIR" -type d -execdir sh -c 'generate_index "$0" > index.html' {} \;
+generate_index "$0" > index.html' {} \;
