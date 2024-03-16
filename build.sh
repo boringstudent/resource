d05d3dbc2db2e51c6daec82c9d1e6b9f153c924c
CSneko <minecrafthetun@outlook.com>
2024-03-16 17:36:55 +0800

    修改脚本

--- build.sh
+++ build.sh
@@ -2,13 +2,17 @@ find -type d -execdir bash -c '
 # 生成目录索引的函数
 generate_index() {
     local dir=$1
+    local path=""
+    if [[ "$dir" != "." ]]; then
+        path="/$dir"
+    fi
     local content="<html>
 <head>
 <meta charset=\"utf-8\">
 <title>$dir</title>
 </head>
 <body>
-<h1>/$dir</h1>
+<h1>$path</h1>
 <hr>"
     
     # 添加上级目录链接
@@ -22,7 +26,7 @@ generate_index() {
         if [[ -d $item ]]; then
             content+="<a href=\"$item/\">$item</a>/ (Directory)<br>"
             # 递归调用生成子目录的索引
-            (cd "$item" && generate_index "/$dir/$item")
+            (cd "$item" && generate_index "$path/$item")
         else
             content+="<a href=\"$item\">$item</a><br>"
         fi
